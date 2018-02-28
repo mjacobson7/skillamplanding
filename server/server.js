1f6597080defe00b101c1044b55690b0ec784d40
maxjacobson <max.jacobson@torchlms.com>
2018-02-28 03:22:01 -0700

    add env.port

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var port = process.env.PORT || 3100;
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.listen(3100);
+app.listen(port);
